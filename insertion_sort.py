331c89895c20b4fc14a7309415f7319f43da3370
leessp <pqs921209@163.com>
2020-06-11 00:40:44 +0800

    求n位二进制数组相加之和

--- insertion_sort.py
+++ insertion_sort.py
@@ -22,7 +22,32 @@ def insertion_sort_desc(a):
         j += 1
 
 a = [5,3,2,7,6,1,9,8,0,7]
+print('before sort',a)
 insertion_sort(a)
 print('asc',a)
 insertion_sort_desc(a)
 print('desc',a)
+
+
+# 计算两个n位二进制整数相加之和
+# A + B = C, C为n+1位
+
+a = [0,1,0]
+b = [1,1,0]
+c = []
+def bit_sum(a, b):
+    k = 0 # 进位标志
+    i = len(a) - 1
+    while i >= 0:
+        sum = a[i]+b[i]+k
+        if sum > 1:
+            k = 1
+            c.insert(0,1)
+        else:
+            k = 0
+            c.insert(0,sum)
+        i -= 1
+    c.insert(0,k)
+
+bit_sum(a,b)
+print('%s + %s = %s' % (a , b, c))
